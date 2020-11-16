3206b51b168fa16b7eb54781cfe32902769dacb9
Leanne Lyons <leannelyons@Leannes-MacBook-Pro.local>
2020-11-16 15:08:29 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO THERE! POOPIE
         </p>
         <a
           className="App-link"
